02ef3a95e4095b064fe9bc5dbe0573bfe1d4271e
Scott Grunert <scott.grunert@landservices.com.au>
2023-12-02 17:43:19 +1030

    day 2

--- src/day2/day2.spec.ts
+++ src/day2/day2.spec.ts
@@ -1,7 +1,13 @@
 import day2 from './index';
 
 describe('On Day 2', () =>{
-    it(`part1 is identity function`, ()=>{
-        expect(day2.solveForPartOne('hello')).toBe('hello');
+    it(`part1`, ()=>{
+        expect(day2.solveForPartOne("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\r\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\r\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\r\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\r\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")).toBe('8');
+        expect(day2.solveForPartOne("Game 1: 3 blue, 7 green, 10 red; 4 green, 4 red; 1 green, 7 blue, 5 red; 8 blue, 10 red; 7 blue, 19 red, 1 green")).toBe("0")
+    })
+
+    it("part 2", () => {
+        expect(day2.solveForPartTwo("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")).toBe("48")
+        expect(day2.solveForPartTwo("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")).toBe("630")
     })
 });
--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,17 +1,77 @@
 import { Day } from "../day";
 
+type Game = {
+    red :number,
+    blue: number,
+    green: number
+}
+
 class Day2 extends Day {
 
     constructor(){
         super(2);
     }
 
+    getGameNumber(row: String) {
+        return parseInt(row.split(":")[0].replace("Game ",""))
+    }
+
+    checkGame(game: Game, max: Game) {
+        return game.red > max.red || game.green > max.green || game.blue > max.blue
+    }
+
+    getMaxGame(game: Game, currentMax: Game) : Game {
+        return {
+            red : Math.max(game.red, currentMax.red),
+            green : Math.max(game.green, currentMax.green),
+            blue : Math.max(game.blue, currentMax.blue),
+        }
+    }
+
+    createGame(game: string) : Game {
+        const initial : Game = { 
+            red: 0,
+            green: 0,
+            blue: 0
+        }
+        game.split(",").forEach(e => {
+            if (e.includes("red")) {
+                initial.red = parseInt(e.replace("red","").trim())
+            }
+            if (e.includes("green")) {
+                initial.green = parseInt(e.replace("green","").trim())
+            }
+            if (e.includes("blue")) {
+                initial.blue = parseInt(e.replace("blue","").trim())
+            }
+        })
+        return initial
+    }
+
     solveForPartOne(input: string): string {
-        return input;
+        const MAX : Game = { red: 12, blue: 14, green: 13 }
+        let sum = 0
+        input.split("\r\n").forEach(  row => {
+            if (!row.split(":")[1].split(";").some(g => this.checkGame(this.createGame(g), MAX))) {
+                sum += this.getGameNumber(row)
+            }
+        })
+        
+        return sum.toString();
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        let sum = 0
+        input.split("\r\n").forEach(  row => {
+            const maxGame = row.split(":")[1]
+            .split(";")
+            .map(gStr => this.createGame(gStr))
+            .reduce((accumulator, currentGame) => {
+                return this.getMaxGame(currentGame, accumulator)
+            }, { red: 0, green: 0, blue: 0})
+            sum += maxGame.red * maxGame.blue * maxGame.green
+        })
+        return sum.toString();
     }
 }
 
